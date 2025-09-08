f205057f6c34a15ea7eae0c9a3b2925846edd21d
chmurka667 <ewa.sapalska@student.uj.edu.pl>
2025-09-08 14:33:11 +0200

    E N D

--- app/src/Form/Type/UserType.php
+++ app/src/Form/Type/UserType.php
@@ -9,6 +9,7 @@ namespace App\Form\Type;
 use App\Entity\User;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
@@ -44,9 +45,10 @@ class UserType extends AbstractType
                 'label' => 'Email',
                 'required' => true,
             ])
-            ->add('password', TextType::class, [
+            ->add('password', PasswordType::class, [
                 'label' => 'Password',
                 'required' => false,
+                'mapped' => false,
             ]);
 
         if ($this->security->isGranted('ROLE_ADMIN')) {
